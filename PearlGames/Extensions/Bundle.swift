5daa052ccffc02672454870017c0d1de8f02c95d
Eugene <56676493+youngblader@users.noreply.github.com>
2023-10-15 21:55:42 +0300

    feat: add UIColor, Bundle extensions

---
+++ PearlGames/Extensions/Bundle.swift
@@ -0,0 +1,18 @@
+//
+//  Bundle.swift
+//  PearlGames
+//
+//  Created by Евгений Зорич on 7.10.23.
+//
+
+import Foundation
+
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+}
---
+++ PearlGames/Extensions/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  PearlGames
+//
+//  Created by Евгений Зорич on 7.10.23.
+//
+
+import Foundation
+
+
+//extension UIColor {
+//    static var tabbarColor = UIco
+//}

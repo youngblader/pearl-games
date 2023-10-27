37a5a777a95ce53a5bcd0e43318ebe9a2c192347
Eugene <56676493+youngblader@users.noreply.github.com>
2023-10-27 17:53:59 +0300

    feat: add UIColor extension

--- PearlGames/Extensions/UIColor.swift
+++ PearlGames/Extensions/UIColor.swift
@@ -6,8 +6,14 @@
 //
 
 import Foundation
+import UIKit
 
-
-//extension UIColor {
-//    static var tabbarColor = UIco
-//}
+extension UIColor {
+    static let lightGray = UIColor(red: 243/255, green: 243/255, blue: 247/255, alpha: 1)
+//    static let lightGray = UIColor(red: 0.57, green: 0.57, blue: 0.57, alpha: 1.00) // new
+    static let textGray = UIColor(red: 92/255, green: 99/255, blue: 96/255, alpha: 1)
+    static let backgroundGray = UIColor(red: 229/255, green: 229/255, blue: 229/255, alpha: 1)
+    static let borderColor = UIColor(red: 213/255, green: 213/255, blue: 213/255, alpha: 1)
+    static let backgroundBlack = UIColor(red: 55/255, green: 53/255, blue: 53/255, alpha: 1)
+    static let lightBlack =  UIColor(red: 0.09, green: 0.10, blue: 0.11, alpha: 1.00)
+}

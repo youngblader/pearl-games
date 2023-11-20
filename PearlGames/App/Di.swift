c34f07afa654847aa6db8cde8cb2786abb8453ba
Eugene <56676493+youngblader@users.noreply.github.com>
2023-11-20 14:06:36 +0300

    feat: add provider at di

--- PearlGames/App/Di.swift
+++ PearlGames/App/Di.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class Di {
-    static let shared = Di()
+    static let shared = Di() // neeed?
 
     //MARK: Services
     private let gameService: GameService
@@ -38,6 +38,22 @@ final class Di {
     
     var searchGamesProvider: SearchGamesProviderImpl {
         return SearchGamesProviderImpl(gameService: gameService, router: router)
+    }
+    
+    var gameDetailsProvider: GameDetailsProviderImpl {
+        return GameDetailsProviderImpl(gameService: gameService, wishlistArchiver: wishlistArchiver, router: router)
+    }
+    
+    var gamesCategoryProvider: GamesCategoryProviderImpl {
+        return GamesCategoryProviderImpl(gameService: gameService, router: router)
+    }
+    
+    var webViewProvider: WebViewProviderImpl {
+        return WebViewProviderImpl(router: router)
+    }
+    
+    var wishlistProvider: WishlistProviderImpl {
+        return WishlistProviderImpl(gameService: gameService, wishlistArchiver: wishlistArchiver, router: router)
     }
 }
 

eec7ffe4ec5c7424794db11ef45201b758556e4b
Eugene <56676493+youngblader@users.noreply.github.com>
2024-02-09 15:18:48 +0300

    feat: add environment variables

--- PearlGames/Networking/API.swift
+++ PearlGames/Networking/API.swift
@@ -7,8 +7,6 @@
 
 import Foundation
 
-fileprivate let key = "6ad384039a514e32aa7c4de0f541e1e3"
-
 protocol API {
     func request<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async throws -> T
 }
@@ -21,8 +19,11 @@ extension API {
         urlComponents.host = endpoint.host
         urlComponents.path = endpoint.path
         urlComponents.queryItems = endpoint.parameters
-        urlComponents.queryItems?.append(URLQueryItem(name: "key", value: key))
         
+        if let key = ProcessInfo.processInfo.environment["API_KEY"] {
+            urlComponents.queryItems?.append(URLQueryItem(name: "key", value: key))
+        }
+
         guard let url = urlComponents.url else {
             throw NetworkError.invalidURL
         }
